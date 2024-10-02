1048ff7971d18a3cf16830ca9cb0d87292c225ae
Bart Van Bos <bart@tetrate.io>
2024-10-02 12:35:04 +0200

    added grafan dashboard

--- info.sh
+++ info.sh
@@ -25,6 +25,16 @@ INGRESS_HTTPS_PORT=$(kubectl get svc istio-ingressgateway -n istio-system -o jso
 NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}' --context "${KUBECONTEXT}")
 print_info "Istio ingress gateway is available at http://${NODE_IP}:${INGRESS_PORT} and https://${NODE_IP}:${INGRESS_HTTPS_PORT}"
 
+# Fetch Prometheus NodePort and Node IP
+print_info "\nFetching Prometheus NodePort information..."
+PROMETHEUS_PORT=$(kubectl get svc prometheus-server -n monitoring -o jsonpath='{.spec.ports[?(@.targetPort==9090)].nodePort}' --context "${KUBECONTEXT}")
+PROMETHEUS_URL="http://${NODE_IP}:${PROMETHEUS_PORT}"
+print_info "Prometheus is available at ${PROMETHEUS_URL}"
+
+print_info "\nFetching Grafana NodePort information..."
+GRAFANA_PORT=$(kubectl get svc grafana -n monitoring -o jsonpath='{.spec.ports[?(@.targetPort==3000)].nodePort}' --context "${KUBECONTEXT}")
+GRAFANA_URL="http://${NODE_IP}:${GRAFANA_PORT}"
+print_info "Grafana is available at ${GRAFANA_URL}"
 print_info "\nFetching Grafana admin password..."
 GRAFANA_ADMIN_PASSWORD=$(kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" --context "${KUBECONTEXT}" | base64 --decode)
 print_info "Grafana admin password: ${GRAFANA_ADMIN_PASSWORD}"
---
+++ monitoring.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+export BASE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${BASE_DIR}/env.sh
+# set -e
+
+
+# Install Prometheus using Helm with NodePort
+function install_prometheus {
+  print_info "Adding Helm repository for Prometheus..."
+  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+  helm repo update
+
+  print_info "Installing Prometheus..."
+  helm upgrade --install prometheus prometheus-community/prometheus \
+    --kube-context "${KUBECONTEXT}" \
+    --namespace monitoring \
+    --create-namespace \
+    --set server.service.type=NodePort \
+    --set "server.service.nodePort=${PROMETHEUS_PORT}" \
+    --wait
+}
+
+# Install Grafana using Helm with NodePort
+function install_grafana {
+  print_info "Adding Helm repository for Grafana..."
+  helm repo add grafana https://grafana.github.io/helm-charts
+  helm repo update
+
+  print_info "Installing Grafana..."
+  helm upgrade --install grafana grafana/grafana \
+    --kube-context "${KUBECONTEXT}" \
+    --namespace monitoring \
+    --set service.type=NodePort \
+    --set "service.nodePort=${GRAFANA_PORT}" \
+    --wait
+}
+
+# Install kube-state-metrics using Helm
+function install_kube_state_metrics {
+  print_info "Installing kube-state-metrics..."
+  helm upgrade --install kube-state-metrics prometheus-community/kube-state-metrics \
+    --kube-context "${KUBECONTEXT}" \
+    --namespace monitoring \
+    --wait
+}
+
+# Function to import a Grafana dashboard using the Grafana API
+function import_dashboard {
+  NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}' --context "${KUBECONTEXT}")
+  GRAFANA_PORT="${GRAFANA_PORT:-30091}"
+  GRAFANA_URL="http://${NODE_IP}:${GRAFANA_PORT}"
+  DASHBOARD_ID=6417
+  GRAFANA_USER="admin"
+  GRAFANA_PASSWORD=$(kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode)
+  
+  print_info "Importing Grafana dashboard with ID ${DASHBOARD_ID}..."
+
+  # Fetch the Grafana dashboard JSON using the dashboard ID
+  DASHBOARD_ID=6417
+  curl -s -H "Accept: application/json" "https://grafana.com/api/dashboards/${DASHBOARD_ID}/revisions/latest/download" -o "${BASE_DIR}/output/dashboard.json"
+
+  # Import the dashboard to Grafana using the Grafana API
+  curl -X POST "${GRAFANA_URL}/api/dashboards/db" \
+    -H "Content-Type: application/json" \
+    -u "${GRAFANA_USER}:${GRAFANA_PASSWORD}" \
+    -d @${BASE_DIR}/output/dashboard.json
+
+  print_info "Dashboard import completed."
+}
+
+# Install Prometheus, kube-state-metrics, and Grafana
+install_prometheus
+install_kube_state_metrics
+install_grafana
+import_dashboard
